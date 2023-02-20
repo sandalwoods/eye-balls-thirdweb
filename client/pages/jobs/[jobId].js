70f05b27316d15e46b49bf4dacadd126b8435fde
sandalwoods <1764076874@qq.com>
2023-02-20 18:41:10 +0800

    show job cost

--- client/pages/jobs/[jobId].js
+++ client/pages/jobs/[jobId].js
@@ -1,17 +1,56 @@
 import { useRouter } from "next/router";
 import Header from "../components/Header";
-import styles from "../../styles/Job.module.css"
-import { useState } from "react";
+import styles from "../../styles/Job.module.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import JobMarketplace from "../../const/JobMarketplace.json";
+import config from "../../const/JobMarketplaceAddress.json";
 
 const Job = () => {
   const router = useRouter();
   const { jobId } = router.query;
   const [account, setAccount] = useState(null);
+  const [job, setJob] = useState(null);
+
+  const getJob = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const network = await provider.getNetwork();
+    const jobMarketplace = new ethers.Contract(
+      config[network.chainId].JobMarketplace[0],
+      JobMarketplace,
+      provider
+    );
+
+    const job = await jobMarketplace.getJob(jobId);
+
+    setJob(job);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
+
+  useEffect(() => {
+    if (jobId != null) {
+      getJob()
+    }    
+  }, [jobId]);
+
+  console.log(job);
 
   return (
     <div className={styles.container}>
       <Header account={account} setAccount={setAccount} />
-      <div>{jobId}</div>
+      <div className={styles.card}>
+        {/* <div className={styles.card__info}>{job?.cost.toString()}</div> */}
+        {/* <div className={styles.card__info}>{ethers.utils.parseEther("1.0")}</div> */}
+        <p>{ethers.utils.formatUnits(job.cost, 'ether')}</p>
+      </div>
     </div>
   );
 };
